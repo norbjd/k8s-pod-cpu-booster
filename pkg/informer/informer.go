ce604c29b7ec5447fd7ea2bfa2d611f2e9cda026
norbjd <norbjd@users.noreply.github.com>
2024-03-16 16:56:11 +0100

    Rewrite informer code logic (only reset boost now)

--- pkg/informer/informer.go
+++ pkg/informer/informer.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
-	"strconv"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/norbjd/k8s-pod-cpu-booster/pkg/shared"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -21,13 +20,7 @@ import (
 )
 
 const (
-	cpuBoostStartupAnnotation = "norbjd.github.io/k8s-pod-cpu-booster-enabled"
-
-	cpuBoostMultiplierAnnotation = "norbjd.github.io/k8s-pod-cpu-booster-multiplier"
-	cpuBoostDefaultMultiplier    = uint64(10)
-
-	cpuBoostContainerNameAnnotation = "norbjd.github.io/k8s-pod-cpu-booster-container"
-
+	cpuBoostStartupLabel         = "norbjd.github.io/k8s-pod-cpu-booster-enabled"
 	cpuBoostProgressLabelName    = "norbjd.github.io/k8s-pod-cpu-booster-progress"
 	cpuBoostInProgressLabelValue = "boosting"
 	cpuBoostDoneLabelValue       = "has-been-boosted"
@@ -60,27 +53,15 @@ func Run(clientset *kubernetes.Clientset) {
 	<-stopper
 }
 
-// only check pods running on the current node, assumes NODE_NAME contains the name of the node
-// only necessary if we want to deploy pod-cpu-booster as a DaemonSet, otherwise a simple Deployment with 1 replica (to avoid conflicts) would be enough
+// only check pods with the CPU boost label set
 func podCPUBoosterTweakFunc() internalinterfaces.TweakListOptionsFunc {
 	return func(opts *metav1.ListOptions) {
-		opts.FieldSelector = "spec.nodeName=" + os.Getenv("NODE_NAME")
+		opts.LabelSelector = metav1.FormatLabelSelector(&metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				cpuBoostStartupLabel: "true",
+			},
+		})
 	}
-}
-
-func getBoostMultiplierFromAnnotations(pod *corev1.Pod) uint64 {
-	if boostMultiplierAnnotationValue, ok := pod.Annotations[cpuBoostMultiplierAnnotation]; ok {
-		boostMultiplierAnnotationValueInt, err := strconv.ParseUint(boostMultiplierAnnotationValue, 10, 64)
-		if err != nil {
-			klog.Errorf("boost multiplier is not a valid value, will take the default %d instead: %s",
-				cpuBoostDefaultMultiplier, err.Error())
-			return cpuBoostDefaultMultiplier
-		}
-
-		return boostMultiplierAnnotationValueInt
-	}
-
-	return cpuBoostDefaultMultiplier
 }
 
 func onUpdate(clientset *kubernetes.Clientset, oldObj interface{}, newObj interface{}) {
@@ -89,60 +70,24 @@ func onUpdate(clientset *kubernetes.Clientset, oldObj interface{}, newObj interf
 	klog.Infof("pod %s/%s updated", newPod.Namespace, newPod.Name)
 	klog.V(9).Info(cmp.Diff(oldPod, newPod))
 
-	if podHasBoostAnnotation(newPod) {
-		if len(newPod.Status.ContainerStatuses) == 0 {
-			klog.Infof("pod %s/%s has no container statuses, skipping...", newPod.Namespace, newPod.Name)
-			return
-		}
+	if len(newPod.Status.ContainerStatuses) == 0 {
+		klog.Infof("pod %s/%s has no container statuses, skipping...", newPod.Namespace, newPod.Name)
+		return
+	}
 
-		containerNameToBoost := newPod.Annotations[cpuBoostContainerNameAnnotation]
+	boostInfo, err := shared.RetrieveBoostInfo(newPod)
+	if err != nil {
+		klog.ErrorS(err, "cannot retrieve boost info")
+		return
+	}
 
-		containerIndex := -1
-
-		if containerNameToBoost == "" {
-			if len(newPod.Spec.Containers) > 1 {
-				klog.Warningf("pod %s/%s contains %d containers but annotation %s is unset, skipping...",
-					newPod.Namespace, newPod.Name, len(newPod.Spec.Containers), cpuBoostContainerNameAnnotation)
-				return
-			} else {
-				containerIndex = 0
-			}
-		} else {
-			for i, container := range newPod.Spec.Containers {
-				if container.Name == containerNameToBoost {
-					containerIndex = i
-					break
-				}
-			}
-
-			if containerIndex == -1 {
-				klog.Warningf("pod %s/%s contains no containers named %s (found in annotation %s), skipping...",
-					newPod.Namespace, newPod.Name, containerNameToBoost, cpuBoostContainerNameAnnotation)
-				return
-			}
-		}
-
-		boostMultiplier := getBoostMultiplierFromAnnotations(newPod)
-
-		if podJustStartedAndNotReadyYet(newPod) {
-			klog.Infof("will boost %s/%s (container %s) CPU limit", newPod.Namespace, newPod.Name, containerNameToBoost)
-			err := boostCPU(clientset, newPod, containerIndex, boostMultiplier)
-			if err != nil {
-				klog.Errorf("error while boosting CPU: %s", err.Error())
-			}
-		} else if podIsNowReadyAfterBoosting(newPod) {
-			klog.Infof("will reset %s/%s (container %s) CPU limit to default", newPod.Namespace, newPod.Name, containerNameToBoost)
-			err := resetCPUBoost(clientset, newPod, containerIndex, boostMultiplier)
-			if err != nil {
-				klog.Errorf("error while resetting CPU boost: %s", err.Error())
-			}
+	if podIsNowReadyAfterBoosting(newPod) {
+		klog.Infof("will reset %s/%s (container %s) CPU limit to default", newPod.Namespace, newPod.Name, boostInfo.ContainerName)
+		err := resetCPUBoost(clientset, newPod, boostInfo.ContainerIndex, boostInfo.Multiplier)
+		if err != nil {
+			klog.Errorf("error while resetting CPU boost: %s", err.Error())
 		}
 	}
-}
-
-func podHasBoostAnnotation(pod *corev1.Pod) bool {
-	boost, ok := pod.Annotations[cpuBoostStartupAnnotation]
-	return ok && boost == "true"
 }
 
 func podIsNowReadyAfterBoosting(newPod *corev1.Pod) bool {
@@ -161,27 +106,6 @@ func podIsNowReadyAfterBoosting(newPod *corev1.Pod) bool {
 	return false
 }
 
-func podJustStartedAndNotReadyYet(pod *corev1.Pod) bool {
-	// we have to wait until it's running before changing the CPU otherwise the behavior is undefined (caught this by experimenting)
-	return pod.Status.Phase == corev1.PodRunning && pod.Labels[cpuBoostProgressLabelName] == ""
-}
-
-func boostCPU(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIndex int, boostMultiplier uint64) error {
-	container := pod.Spec.Containers[containerIndex]
-	currentCPULimit := container.Resources.Limits.Cpu()
-	cpuLimitAfterBoost := resource.NewScaledQuantity(currentCPULimit.ScaledValue(resource.Nano)*int64(boostMultiplier), resource.Nano)
-
-	klog.Infof("Current CPU limit for %s/%s (container %s) is %s, will set new CPU limit to %s",
-		pod.Namespace, pod.Name, container.Name, currentCPULimit, cpuLimitAfterBoost)
-
-	err := writeCPULimit(clientset, pod, containerIndex, cpuLimitAfterBoost, boost)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func resetCPUBoost(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIndex int, boostMultiplier uint64) error {
 	container := pod.Spec.Containers[containerIndex]
 	currentCPULimit := container.Resources.Limits.Cpu()
@@ -190,7 +114,7 @@ func resetCPUBoost(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIn
 	klog.Infof("Current CPU limit for %s/%s (container %s) is %s, will reset CPU limit to %s",
 		pod.Namespace, pod.Name, container.Name, currentCPULimit, cpuLimitAfterReset)
 
-	err := writeCPULimit(clientset, pod, containerIndex, cpuLimitAfterReset, reset)
+	err := writeCPULimit(clientset, pod, containerIndex, cpuLimitAfterReset)
 	if err != nil {
 		return err
 	}
@@ -198,14 +122,7 @@ func resetCPUBoost(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIn
 	return nil
 }
 
-type action int32
-
-const (
-	boost action = iota
-	reset
-)
-
-func writeCPULimit(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIndex int, cpuLimit *resource.Quantity, action action) error {
+func writeCPULimit(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIndex int, cpuLimit *resource.Quantity) error {
 	ctx := context.Background()
 	podsClient := clientset.CoreV1().Pods(pod.Namespace)
 
@@ -215,25 +132,7 @@ func writeCPULimit(clientset *kubernetes.Clientset, pod *corev1.Pod, containerIn
 			return fmt.Errorf("failed to get latest version of pod %s/%s: %v", pod.Namespace, pod.Name, getErr)
 		}
 
-		if action == boost && result.Labels[cpuBoostProgressLabelName] == cpuBoostInProgressLabelValue {
-			klog.Info("Already in boosting process, skipping...")
-			return nil
-		}
-
-		switch action {
-		case boost:
-			if result.Labels == nil {
-				result.Labels = make(map[string]string)
-			}
-			result.Labels[cpuBoostProgressLabelName] = cpuBoostInProgressLabelValue
-		case reset:
-			if result.Labels == nil {
-				result.Labels = make(map[string]string)
-			}
-			result.Labels[cpuBoostProgressLabelName] = cpuBoostDoneLabelValue
-		default:
-			return fmt.Errorf("unknown action: %d (expected %d or %d)", action, boost, reset)
-		}
+		result.Labels[cpuBoostProgressLabelName] = cpuBoostDoneLabelValue
 
 		container := result.Spec.Containers[containerIndex]
 
